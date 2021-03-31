86326dc9587a47d215cba5afe404a1f8136978f3
Pahukun <77877113+pahukun@users.noreply.github.com>
2021-03-31 10:54:10 +0700

    copy files

---
+++ StudyWithPSea/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace StudyWithPSea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.txtLng = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(1420, 684);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.map_OnMarkerClick);
+            this.map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.map_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.Pink;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtLng);
+            this.panel1.Controls.Add(this.txtLat);
+            this.panel1.Location = new System.Drawing.Point(46, 436);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(240, 195);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtLat
+            // 
+            this.txtLat.ForeColor = System.Drawing.Color.Black;
+            this.txtLat.Location = new System.Drawing.Point(76, 19);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(149, 20);
+            this.txtLat.TabIndex = 0;
+            // 
+            // txtLng
+            // 
+            this.txtLng.ForeColor = System.Drawing.Color.Black;
+            this.txtLng.Location = new System.Drawing.Point(76, 58);
+            this.txtLng.Name = "txtLng";
+            this.txtLng.Size = new System.Drawing.Size(149, 20);
+            this.txtLng.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.HotPink;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(13, 142);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(212, 31);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add Marker";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lat :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Long :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name :";
+            // 
+            // txtName
+            // 
+            this.txtName.ForeColor = System.Drawing.Color.Black;
+            this.txtName.Location = new System.Drawing.Point(76, 96);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1420, 684);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtLng;
+        private System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ StudyWithPSea/Form1.cs
@@ -0,0 +1,84 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyWithPSea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        GMapOverlay overlay = new GMapOverlay("testOverlay"); //เป็นการสร้างตัวแปร overlay ขึ้นมา ใน "" เป็นชื่ออะไรก็ได้
+        Dictionary<GMapMarker, MarkerDetail> markers = new Dictionary<GMapMarker, MarkerDetail>();
+        bool isUpdate = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            map.MaxZoom = 15; //ระยะการซูมใกล้สูงสุด
+            map.MinZoom = 5; //ระยะการซูมไกลสูงสุด
+            map.Zoom = 8; //ระยะการซูมเริ้มต้น
+            map.Manager.Mode = AccessMode.ServerOnly; //เป็นการเลือกโหมกว่าจะดึงแมพจากไหน Server หรือ เครื่องเราเอง
+            map.MapProvider = GMapProviders.GoogleMap; //เป็นรูปแบบของแมพ
+
+            map.Overlays.Add(overlay); //เป็นการสร้าง overlay ขึ้นมาบนแมพ 1 ชั้น
+        }
+
+        private void map_MouseClick(object sender, MouseEventArgs e)
+        {
+           isUpdate = false;
+           
+           PointLatLng point =  map.FromLocalToLatLng(e.X, e.Y); //ดึงค่า lat lng จาด map มาใส่ไว้ในตัวแปร
+           if (e.Button == MouseButtons.Left) //ดัก Event คลิกซ้าย
+           {
+                Console.WriteLine(isUpdate);
+                txtLat.Text = point.Lat.ToString();
+                txtLng.Text = point.Lng.ToString();    
+           }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double lat = double.Parse(txtLat.Text);
+            double lng = double.Parse(txtLng.Text);
+            PointLatLng pointLatLng = new PointLatLng(lat, lng); //เอาค่า lat lng จาก txt มาใส่ในตัวแปร
+            GMapMarker marker = new GMarkerGoogle(pointLatLng, GMarkerGoogleType.pink); //สร้างมาร์คเกอร์เข้าตัวแปร
+            MarkerDetail detail = new MarkerDetail();
+            detail.id = 0;
+            detail.name = txtName.Text;
+            markers.Add(marker, detail);
+            marker.ToolTipText = detail.id + "\n" + detail.name; //ดึงข้อความมาใส่ใน marker.ToolTipText
+            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver; //ไว้แสดงข้อความตอนเมาส์ไป over ไว้
+            overlay.Markers.Add(marker); //แอดมาร์คเกอร์ลงบน Overlay
+            txtLat.Text = "";
+            txtLng.Text = "";
+            txtName.Text = "";
+        }
+
+        private void map_OnMarkerClick(GMapMarker item, MouseEventArgs e) //Event เมื่อเอาเม้าส์ไปคลิกที่ marker
+        {
+            isUpdate = true;
+            Console.WriteLine(isUpdate);
+            PointLatLng pointData = item.Position;
+            if (e.Button == MouseButtons.Left) //ดัก Event คลิกซ้าย
+            {
+                txtLat.Text = pointData.Lat.ToString();
+                txtLng.Text = pointData.Lng.ToString();
+                txtName.Text = markers[item].name;
+            }
+        }
+    }
+}
